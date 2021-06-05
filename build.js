4949bb2f3143f3df3adb0323c86353223e25a05c
Timo Scheuermann <timo.scheuermann@gmx.de>
2021-06-05 14:24:10 +0200

    fixed build

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
+const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
 
 const file = readFileSync(join(__dirname, 'package.json')).toString('utf-8');
 
-const packageInfomration = JSON.parse(file);
+const packageInformation = JSON.parse(file);
 
-const version = packageInfomration.version;
+const version = packageInformation.version;
 
 writeFileSync(join(__dirname, '.env'), `VUE_APP_VERSION=v${version}`);
